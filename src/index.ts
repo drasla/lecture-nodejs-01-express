e0117b183153f409295914a88d0e57bdd973c591
Tae.Garam (Home) <drasla217@gmail.com>
2026-03-29 22:20:25 +0900

    정적 페이지로 로그인 기능 구현

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { type Request, type Response } from "express";
 import dotenv from "dotenv";
 import postRouter from "./routes/postRouter";
 import userRouter from "./routes/userRouter";
+import path from "path";
 
 // 1. 환경 변수 초기화
 dotenv.config();
@@ -14,9 +15,12 @@ const PORT = process.env.PORT || 8000;
 // JSON 형태의 데이터를 주고받기 위해 꼭 필요합니다.
 app.use(express.json());
 
+// 4. 정적 파일 제공할 폴더 지정
+app.use(express.static(path.join(process.cwd(), "public")));
+
 // 기본 라우트
 app.get("/", (req: Request, res: Response) => {
-    res.send("🚀 Express 서버가 정상적으로 실행 중입니다!");
+    res.sendFile(path.join(process.cwd(), "public", "index.html"));
 });
 app.use("/posts", postRouter);
 app.use("/users", userRouter);
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 
 const router = express.Router();
 
@@ -12,29 +13,24 @@ router.get("/login", (req, res) => {
     // 2. 요청 데이터 유효성 검사
     // 3. mockUsers 중 입력된 email을 가진 계정이 있는지 검색
     // 4. 입력된 비밀번호와 일치하는지 확인
-    const { email, password } = req.body;
+    const { email, password } = req.query;
+    const successPagePath = path.join(process.cwd(), "public", "result.html");
+    const failPagePath = path.join(process.cwd(), "public", "fail.html");
 
     if (!email || !password) {
-        return res.status(400).json({ message: "이메일과 비밀번호를 모두 입력해주세요." });
+        return res.sendFile(failPagePath);
     }
 
     const user = mockUsers.find(u => u.email === email);
     if (!user) {
-        return res.status(404).json({ message: "가입되지 않은 이메일입니다." });
+        return res.sendFile(failPagePath);
     }
 
     if (user.password !== password) {
-        return res.status(401).json({ message: "비밀번호가 일치하지 않습니다. " });
+        return res.sendFile(failPagePath);
     }
 
-    res.status(200).json({
-        message: `${user.name}님 환영합니다!`,
-        data: {
-            id: user.id,
-            email: user.email,
-            name: user.name,
-        },
-    });
+    res.sendFile(successPagePath);
 });
 
 export default router;
